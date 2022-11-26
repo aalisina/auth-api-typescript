e69c7f623da29dde1dfbf692cb26f3370447a06c
sinazay123 <sinazay123@gmail.com>
2022-11-26 11:15:04 -0500

    started an express server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log("App started");
+require("dotenv").config();
+import express from "express";
+import config from "config";
+
+const app = express();
+
+const port = config.get("port");
+
+app.listen(() => {
+  console.log(`App started at http://localhost:${port}`);
+});
