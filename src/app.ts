8c5dd882bcfaef9cca256f6d7b34e47397539447
sinazay123 <sinazay123@gmail.com>
2022-11-26 12:17:40 -0500

    Added router

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import express from "express";
 import config from "config";
 import connectToDB from "./utils/connectToDb";
 import log from "./utils/logger";
+import router from "./routes";
 
 const app = express();
 
+app.use(router);
 const port = config.get("port");
 
 app.listen(() => {
