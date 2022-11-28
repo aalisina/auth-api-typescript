fa7186e7768b839a1441cbcf1bee5f280cd9086f
sinazay123 <sinazay123@gmail.com>
2022-11-28 11:54:02 -0500

    added test config for the smtp mailer

--- config/default.ts
+++ config/default.ts
@@ -2,4 +2,11 @@ export default {
   port: 3000,
   dbUri: "mongodb://localhost:27017/auth-api",
   logLevel: "info",
+  smtp: {
+    user: "icqskc5mxzf3bwct@ethereal.email",
+    pass: "mVSU79SbF3WmvGqy4V",
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // set to true when deploy
+  },
 };
