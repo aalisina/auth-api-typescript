9cf9442b06f039de4e55b80e97d182ee24cce3c2
sinazay123 <sinazay123@gmail.com>
2022-12-02 14:09:45 -0500

    Added route for resetting the user's password

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,12 +2,14 @@ import express from "express";
 import {
   createUserHandler,
   forgotPasswordHandler,
+  resetPasswordHandler,
   verifyUserHandler,
 } from "../controllers/user.controller";
 import validateResource from "../middlewares/validateResource";
 import {
   createUserSchema,
   forgotPasswordSchema,
+  resetPasswordSchema,
   verifyUserSchema,
 } from "../schemas/user.schema";
 
@@ -31,4 +33,9 @@ router.post(
   forgotPasswordHandler
 );
 
+router.post(
+  "/api/users/resetpassword/:id/:passwordResetCode",
+  validateResource(resetPasswordSchema),
+  resetPasswordHandler
+);
 export default router;
