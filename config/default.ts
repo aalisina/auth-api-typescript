7d25e063298aaf3e78244df59144ca057e7d22c3
sinazay123 <sinazay123@gmail.com>
2022-12-03 09:54:28 -0500

    Added accessTokenPrivateKey and refreshTokenPrivateKey and set them to empty strings

--- config/default.ts
+++ config/default.ts
@@ -2,6 +2,9 @@ export default {
   port: 3000,
   dbUri: "mongodb://localhost:27017/auth-api",
   logLevel: "info",
+  accessTokenPrivateKey: "",
+  refreshTokenPrivateKey: "",
+
   smtp: {
     user: "a5svnklpvm25d34x@ethereal.email",
     pass: "bPy2rasppnKK6Sux1N",
