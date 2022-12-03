eb8453a29f1cbe513b65093f3679bda0bbebd508
sinazay123 <sinazay123@gmail.com>
2022-12-03 10:00:31 -0500

    Added env variables for the jwt keys

---
+++ config/custom-environment-variables.ts
@@ -0,0 +1,8 @@
+// This file will get the environment variables
+
+export default {
+  accessTokenPrivateKey: "ACCESS_TOKEN_PRIVATE_KEY",
+  refreshTokenPrivateKey: "REFRESH_PRIVATE_KEY",
+  accessTokenPublicKey: "ACCESS_TOKEN_PUBLIC_KEY",
+  refreshTokenPublicKey: "REFRESH_PUBLIC_KEY",
+};
