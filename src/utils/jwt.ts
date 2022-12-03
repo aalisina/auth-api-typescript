43c1c359e3761cec5c33d40175ff7499aa8b0bee
sinazay123 <sinazay123@gmail.com>
2022-12-03 10:07:56 -0500

    Added a verifyJwt function to verify tokens

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -16,4 +16,18 @@ export function signJwt(
   });
 }
 
-export function verifyJwt() {}
+export function verifyJwt<T>(
+  token: string,
+  keyName: "accessTokenPublicKey" | "refreshTokenPublicKey"
+): T | null {
+  const publicKey = Buffer.from(config.get<string>(keyName), "base64").toString(
+    "ascii"
+  );
+
+  try {
+    const decoded = jwt.verify(token, publicKey) as T;
+    return decoded;
+  } catch (err) {
+    return null;
+  }
+}
