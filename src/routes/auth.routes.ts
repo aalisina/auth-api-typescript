cd0dec93226da34e10a0b8527e8446ab3d609948
asalizay <sinazay123@gmail.com>
2022-12-05 18:54:01 +0100

    Added route and middleware for creating a session

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,2 +1,14 @@
-const authroutes = {};
-export default authroutes;
+import express from "express";
+import { createSessionHandler } from "../controllers/auth.controller";
+import validateResource from "../middlewares/validateResource";
+import { createSessionSchema } from "../schemas/auth.schema";
+
+const router = express.Router();
+
+router.post(
+  "/api/sessions",
+  validateResource(createSessionSchema),
+  createSessionHandler
+);
+
+export default router;
