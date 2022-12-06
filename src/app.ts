ccd08e948f2744b6cc146e4f58e929a72ac04f4c
sinazay123 <sinazay123@gmail.com>
2022-12-06 10:41:29 -0500

    Added the deserializeUser middleware above the routes

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,12 @@ import config from "config";
 import connectToDB from "./utils/connectToDb";
 import log from "./utils/logger";
 import router from "./routes";
+import deserializeUser from "./middlewares/deserializeUser";
 
 const app = express();
 
 app.use(express.json());
+app.use(deserializeUser); // must be above the router
 app.use(router);
 const port = config.get("port");
 
