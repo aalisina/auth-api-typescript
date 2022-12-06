59e88c7ebec5fc98dd64e43e4fc8250760f2461e
sinazay123 <sinazay123@gmail.com>
2022-12-06 10:51:06 -0500

    Added an array of privatefields that we want to exclude when we sign a jwt token

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -11,6 +11,14 @@ import { v4 as uuidv4 } from "uuid";
 import argon2 from "argon2";
 import log from "../utils/logger";
 
+export const privateFields = [
+  "password",
+  "__v",
+  "verificationCode",
+  "passwordResetCode",
+  "verified",
+];
+
 @pre<User>("save", async function () {
   if (!this.isModified("password")) {
     return;
